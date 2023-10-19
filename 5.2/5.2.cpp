364ed79e4722884eb8ee4fab6f4531e6d6caf467
Тимур Платонов <gered372@gmail.com>
2023-10-19 20:28:29 +0300

    Добавьте файлы проекта.

---
+++ 5.2/5.2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main() {
+	int a;
+	cin >> a;
+	for (int i = 2; i <= a; i++) {
+		bool fl = true;
+		for (int j = 2; j <= sqrt(i); j++) {
+			if (i % j == 0) {
+				fl = false;
+			}
+		}
+		if (fl) {
+			cout << i << endl;
+		}
+	}
+
+}
